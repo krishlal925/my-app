bca05b355984675b2c1d89d98eb8e71f473bb2d4
Neel <krishlal925@gmail.com>
2020-01-14 18:31:05 -0800

    2nd commit

--- App.js
+++ App.js
@@ -6,17 +6,38 @@ const {users, companies} = data;
 
 console.log(companies);
 
+const companiesByState = companies.reduce((acc,company) => {
+  acc[company.state] = acc[company.state] || [];
+  acc[company.state].push(company);
+  return acc;
+}, {})
+console.log(companiesByState);
 function App() {
   const foo = 'bar';
   console.log("test#4")
   return (
     <div>
       <h1> Hello World!! {foo.toUpperCase()}</h1>
+       <ul>
+         {
+           Object.entries(companiesByState).map(entry => {
+             return (
+               <li key= {entry[0]}>
+                 {entry[0]}
+                 ({entry[1].length})
+               </li>
+             )
+           })
+         }
+       </ul>
+
         {
           users.map(user => {
+
             return(
               <li key={user.id} >
                 {user.name} from: {user.state}
+                works for  { companies.find(company =>company.id === user.CompanyId).name}
               </li>
             );
           })
--- data.js
+++ data.js
@@ -3,29 +3,32 @@ import faker from 'faker';
 const users = [];
 const companies = [];
 
-console.log("test#2")
+console.log("test#2");
 
-while(companies.length < 10){
-  users.push({
+
+while(companies.length < 20){
+  companies.push({
     name: faker.company.companyName(),
     id: companies.length +1,
     state: faker.address.state()
   })
-  console.log( companies.length);
+
 }
-console.log("test #3")
+
 
 while(users.length < 100){
   users.push({
     name: faker.name.firstName(),
     id: users.length +1,
-    state: faker.address.state()
+    state: faker.address.state(),
+    CompanyId: faker.random.arrayElement(companies).id
   })
 }
-console("test#4")
+console.log("test#4")
 
 const data = {
   users,
   companies
 }
 export default data;
+
